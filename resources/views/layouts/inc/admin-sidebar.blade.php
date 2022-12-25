1522bf166a68ca4e54e27feafb980f25760191d2
davengahu007 <davengahu007@gmail.com>
2022-12-26 00:06:03 +0300

    Yes

--- resources/views/layouts/inc/admin-sidebar.blade.php
+++ resources/views/layouts/inc/admin-sidebar.blade.php
@@ -9,12 +9,12 @@
                             </a>
                             <div class="sb-sidenav-menu-heading">Actions</div>
         
-                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts2" aria-expanded="false" aria-controls="collapseLayouts">
+                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts0" aria-expanded="false" aria-controls="collapseLayouts">
                                 <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
                                 Courses
                                 <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                             </a>
-                            <div class="collapse" id="collapseLayouts2" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                            <div class="collapse" id="collapseLayouts0" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                                 <nav class="sb-sidenav-menu-nested nav">
                                     <a class="nav-link" href="{{url('admin/viewcourse/')}}">View Courses</a>
                                     <a class="nav-link" href="{{url('admin/addcourse/')}}">Add Courses</a>
@@ -23,30 +23,34 @@
                     
                             <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts1" aria-expanded="false" aria-controls="collapseLayouts">
                                 <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                                Leave Type
+                                Units
                                 <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                             </a>
                             <div class="collapse" id="collapseLayouts1" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                                 <nav class="sb-sidenav-menu-nested nav">
-                                    <a class="nav-link" href="#">New Leave Type</a>
-                                    <a class="nav-link" href="#">Leave type List</a>
+                                    <a class="nav-link" href="{{url('admin/viewunit/')}}">View Units</a>
+                                    <a class="nav-link" href="{{url('admin/addunit/')}}">Add Unit</a>
                                 </nav>
                             </div>
+
+
                             <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts2" aria-expanded="false" aria-controls="collapseLayouts">
                                 <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                                Staff
+                                 Applications
                                 <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                             </a>
                             <div class="collapse" id="collapseLayouts2" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                                 <nav class="sb-sidenav-menu-nested nav">
-                                    <a class="nav-link" href="#">New Staff</a>
-                                    <a class="nav-link" href="#">Manage Staff</a>
-                                    <a class="nav-link" href="#">Manage HOD</a>
-                                    <a class="nav-link" href="#">Employees on Leave</a>
-                                    <a class="nav-link" href="#">Employees at Work</a>
-                                    <a class="nav-link" href="#">HOD's at Work</a>
+                                    <a class="nav-link" href="{{url('admin/student_pending/')}}">Student Appications</a>
+                                    <a class="nav-link" href="{{url('admin/worker_pending/')}}">Staff Applications</a>
                                 </nav>
                             </div>
+
+
+                            
+
+
+
                             <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts3" aria-expanded="false" aria-controls="collapseLayouts">
                                 <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
                                 Leaves
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -32,7 +32,7 @@
 
 
 
-{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
 <script src="{{ asset('assets/js/owl.carousel.min.js')}}"></script>
 <script src="{{ asset('assets/js/scripts.js')}}"></script>
@@ -42,7 +42,7 @@
 <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
 <script src="js/datatables-simple-demo.js"></script>
 
-@yield('scripts') --}}
+@yield('scripts')
 
 </body>
 </html>
